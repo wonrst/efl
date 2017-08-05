dd95caadc9101d3804b912d24c8a06f58486c1d7
Stefan Schmidt <stefan@osg.samsung.com>
2017-08-05 13:40:20 +0200

    ci: document more potential build options for the misc build

--- .ci/ci-linux-build-misc.sh
+++ .ci/ci-linux-build-misc.sh
@@ -3,7 +3,9 @@
 COPTS="--with-tests=regular --enable-harfbuzz --enable-liblz4 --enable-image-loader-webp --enable-xinput22 --enable-xine --enable-multisense --enable-lua-old --enable-xpresent --enable-hyphen"
 PARALLEL_JOBS=10
 
-# --enable-libvlc
+# --enable-libvlc --enable-vnc-server --enable-g-main-loop --enable-libuv --enable-fb --enable-eglfs --enable-sdl --enable-gl-drm --enable-egl --enable-pixman --enable-tile-rotate --enable-ecore-buffer --enable-image-loader-generic --enable-image-loader-jp2k --enable-gesture --enable-v4l2
+
+# --with-profile=PROFILE --with-crypto=CRYPTO
 
 # Normal build test of all targets
 ./autogen.sh $COPTS $@
