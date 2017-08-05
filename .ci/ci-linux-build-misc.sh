d2dc2c1526c6e1ba5cb0b33d16139fcce1139538
Stefan Schmidt <stefan@osg.samsung.com>
2017-08-05 13:30:38 +0200

    ci: disable vlc from misc build as its not by default in fedora

--- .ci/ci-linux-build-misc.sh
+++ .ci/ci-linux-build-misc.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-COPTS="--with-tests=regular --enable-harfbuzz --enable-liblz4 --enable-image-loader-webp --enable-xinput22 --enable-xine --enable-multisense --enable-lua-old --enable-libvlc --enable-xpresent --enable-hyphen"
+COPTS="--with-tests=regular --enable-harfbuzz --enable-liblz4 --enable-image-loader-webp --enable-xinput22 --enable-xine --enable-multisense --enable-lua-old --enable-xpresent --enable-hyphen"
 PARALLEL_JOBS=10
+
+# --enable-libvlc
 
 # Normal build test of all targets
 ./autogen.sh $COPTS $@
