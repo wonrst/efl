96a9a4f402c5f477d6501d03a9fabbb6ad3e672b
Stefan Schmidt <stefan@osg.samsung.com>
2017-08-07 17:26:53 +0200

    ci: install old lua and disable xine for misc build

--- .ci/ci-linux-build-misc.sh
+++ .ci/ci-linux-build-misc.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-COPTS="--with-tests=regular --enable-harfbuzz --enable-liblz4 --enable-image-loader-webp --enable-xinput22 --enable-xine --enable-multisense --enable-lua-old --enable-xpresent --enable-hyphen"
+COPTS="--with-tests=regular --enable-harfbuzz --enable-liblz4 --enable-image-loader-webp --enable-xinput22 --enable-multisense --enable-lua-old --enable-xpresent --enable-hyphen"
 PARALLEL_JOBS=10
 
-# --enable-libvlc --enable-vnc-server --enable-g-main-loop --enable-libuv --enable-fb --enable-eglfs --enable-sdl --enable-gl-drm --enable-egl --enable-pixman --enable-tile-rotate --enable-ecore-buffer --enable-image-loader-generic --enable-image-loader-jp2k --enable-gesture --enable-v4l2
+# --enable-libvlc --enable-vnc-server --enable-g-main-loop --enable-libuv --enable-fb --enable-eglfs --enable-sdl --enable-gl-drm --enable-egl --enable-pixman --enable-tile-rotate --enable-ecore-buffer --enable-image-loader-generic --enable-image-loader-jp2k --enable-gesture --enable-v4l2 --enable-xine
 
 # --with-profile=PROFILE --with-crypto=CRYPTO
 
